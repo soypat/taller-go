67725b34424f1203003d9850c9f597c309d6c300
agust <acanalis@itba.edu.ar>
2020-11-03 14:10:10 -0300

    Add script to build slides to static site

---
+++ buildstatic.sh
@@ -0,0 +1,17 @@
+present.exe -http 127.0.0.1:3999 -use_playground true &
+
+curl -s http://127.0.0.1:3999/static/notes.js > static/notes.js
+curl -s http://127.0.0.1:3999/static/slides.js | sed -E 's/(\/static\/)/..\/static\//' > static/slides.js
+curl -s http://127.0.0.1:3999/static/styles.css > static/styles.css
+curl -s http://127.0.0.1:3999/static/notes.css > static/notes.css
+# not working:
+# curl -s http://127.0.0.1:3999/static/play.js > static/play.js
+
+
+curl -s http://127.0.0.1:3999/go-desde-0/desde-0.slide | sed -E 's/(\/static\/)/..\/static\//' | sed -E 's/\/play.js/..\/static\/play.js/' > go-desde-0/index.html
+curl -s http://127.0.0.1:3999/go-efectivo/go-efectivo.slide | sed -E 's/(\/static\/)/..\/static\//' | sed -E 's/\/play.js/..\/static\/play.js/'> go-efectivo/index.html
+curl -s http://127.0.0.1:3999/go-slides-example/sample.slide | sed -E 's/(\/static\/)/..\/static\//'| sed -E 's/\/play.js/..\/static\/play.js/'> go-slides-example/index.html
+
+kill $!
+
+echo "To see result run\n python3 -m http.server"
