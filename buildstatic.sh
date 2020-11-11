de9ee72243afb271fa5b2d60f3d7ead45e7b3cec
soypat <graded.sp@gmail.com>
2020-11-11 11:11:38 -0300

    fixed static builder

--- buildstatic.sh
+++ buildstatic.sh
@@ -1,4 +1,7 @@
 export DEPLOY="./deploy"
+# so if a port is blocked, no problem, just use different
+export PORT="3998"
+rm -rf deploy/static
 mkdir -p deploy/static 
 mkdir -p deploy/go-desde-0
 mkdir -p deploy/go-efectivo
@@ -7,34 +10,34 @@ mkdir -p deploy/assets
 rm -rf deploy/assets
 cp -r assets deploy/assets
 
-go-presentx -base . -http 127.0.0.1:3999 -use_playground &
+go-presentx -base . -http 127.0.0.1:${PORT} -use_playground &
 
-# curl -s http://127.0.0.1:3999/static/notes.js > ${DEPLOY}/static/notes.js
-# curl -s http://127.0.0.1:3999/static/slides.js | sed -E 's/(\/static\/)/..\/static\//' > ${DEPLOY}/static/slides.js
-curl -s http://127.0.0.1:3999/static/styles.css > ${DEPLOY}/static/styles.css
-curl -s http://127.0.0.1:3999/static/article.css > ${DEPLOY}/static/article.css
-curl -s http://127.0.0.1:3999/static/dir.css > ${DEPLOY}/static/dir.css
-curl -s http://127.0.0.1:3999/static/prism-github.dark.css > ${DEPLOY}/static/styles.css
-curl -s http://127.0.0.1:3999/static/notes.css > ${DEPLOY}/static/notes.css
+# css
+curl -s http://127.0.0.1:${PORT}/static/styles.css > ${DEPLOY}/static/styles.css
+curl -s http://127.0.0.1:${PORT}/static/article.css > ${DEPLOY}/static/article.css
+curl -s http://127.0.0.1:${PORT}/static/dir.css > ${DEPLOY}/static/dir.css
+curl -s http://127.0.0.1:${PORT}/static/prism-github-dark.css > ${DEPLOY}/static/prism-github-dark.css
+curl -s http://127.0.0.1:${PORT}/static/notes.css > ${DEPLOY}/static/notes.css
 
-
-echo $REPL
-curl -s http://127.0.0.1:3999/play.js | sed  > ${DEPLOY}/static/play.js
+# Javascript
+curl -s http://127.0.0.1:${PORT}/static/notes.js > ${DEPLOY}/static/notes.js
+curl -s http://127.0.0.1:${PORT}/static/dir.js > ${DEPLOY}/static/dir.js
+curl -s http://127.0.0.1:${PORT}/static/slides.js > ${DEPLOY}/static/slides.js
+curl -s http://127.0.0.1:${PORT}/play.js > ${DEPLOY}/play.js
 # works, but replace in line 124: (best attempt:)'\$\.ajax\(\'\\compile\', \{ /\$\.ajax\(\{url: \'https:\/\/play\.golang\.org\/compile\', /g'
 # '$.ajax('\compile', { 
 # with:
 # $.ajax({url: 'https:\/\/play.golang.org\/compile', 
 # base
-curl -s http://127.0.0.1:3999/ > ${DEPLOY}/index.html
+curl -s http://127.0.0.1:${PORT}/ > ${DEPLOY}/index.html
 # folders (replace .slide with html)
-curl -s http://127.0.0.1:3999/go-desde-0/ | sed 's/.slide">/.html"/g' > ${DEPLOY}/go-desde-0/index.html
-curl -s http://127.0.0.1:3999/go-efectivo/ | sed 's/.slide">/.html"/g' > ${DEPLOY}/go-efectivo/index.html
-curl -s http://127.0.0.1:3999/go-slides-example/ | sed 's/.slide">/.html"/g' > ${DEPLOY}/go-slides-example/index.html
+curl -s http://127.0.0.1:${PORT}/go-desde-0/ | sed 's/.slide">/.html"/g' > ${DEPLOY}/go-desde-0/index.html
+curl -s http://127.0.0.1:${PORT}/go-efectivo/ | sed 's/.slide">/.html"/g' > ${DEPLOY}/go-efectivo/index.html
+curl -s http://127.0.0.1:${PORT}/go-slides-example/ | sed 's/.slide">/.html"/g' > ${DEPLOY}/go-slides-example/index.html
 # Presentations
-curl -s http://127.0.0.1:3999/go-desde-0/desde-0.slide  > ${DEPLOY}/go-desde-0/desde-0.html
-curl -s http://127.0.0.1:3999/go-efectivo/go-efectivo.slide > ${DEPLOY}/go-efectivo/go-efectivo.html
-curl -s http://127.0.0.1:3999/go-slides-example/sample.slide > ${DEPLOY}/go-slides-example/sample.html
+curl -s http://127.0.0.1:${PORT}/go-desde-0/desde-0.slide  > ${DEPLOY}/go-desde-0/desde-0.html
+curl -s http://127.0.0.1:${PORT}/go-efectivo/go-efectivo.slide > ${DEPLOY}/go-efectivo/go-efectivo.html
+curl -s http://127.0.0.1:${PORT}/go-slides-example/sample.slide > ${DEPLOY}/go-slides-example/sample.html
 
 kill $!
-
-echo "To see result run\n python3 -m http.server"
+echo "you can now run `sv` or `python3 -m http-server in ./deploy"
