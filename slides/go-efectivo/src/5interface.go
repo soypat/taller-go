2d9a5a038f47664a5bb58016b8e2aea64b850b4f
agust <acanalis@itba.edu.ar>
2020-12-02 19:41:22 -0300

    Add io example to interfaces

--- slides/go-efectivo/src/5interface.go
+++ slides/go-efectivo/src/5interface.go
@@ -1,63 +1,41 @@
 //+build os OMIT
 
+// ALL_S OMIT
 package main
 
 import (
 	"fmt"
-	"sort"
+	"io"
+	"io/ioutil"
+	"os"
 )
+
+func main() {
+	f, err := os.Open("slides/go-efectivo/src/hello.txt") // f es un *os.File 
+	if err != nil {
+		panic(err)
+	}
+	b, err := ioutil.ReadAll(f) // ReadAll acepta a f como argumento
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", b)
+}
+
+// ALL_E OMIT
 
 // INTERFACE_S OMIT
 // Dentro del paquete io
 type Reader interface {
 	Read(p []byte) (n int, err error)
 }
+
 // INTERFACE_E OMIT
 
 // READALL_S OMIT
 // Dentro del paquete ioutil
-func ReadAll(r io.Reader) ([]byte, error) {}
+func ReadAll(r io.Reader) ([]byte, error) {
+	return nil, nil
+}
+
 // READALL_E OMIT
-
-type Writer interface {
-	Write(p []byte) (n int, err error)
-}
-
-
-// USG_S OMIT
-type Names []string
-
-func (a Names) Len() int {
-	return len(a)
-}
-func (a Names) Swap(i, j int) {
-	a[i], a[j] = a[j], a[i]
-}
-func (a Names) Less(i, j int) bool {
-	return a[i] < a[j]
-}
-
-func main() {
-	var nombres sort.Interface = Names{"Paco", "Yorsch", "Quique", "Cana", "Pato"}
-	fmt.Println(nombres)
-	sort.Sort(nombres)
-	fmt.Println(nombres)
-}
-
-// USG_E OMIT
-
-// SORTER_S OMIT
-type Interface interface {
-	Len() int
-	Less(i, j int) bool
-	Swap(i, j int)
-}
-
-// SORTER_E OMIT
-
-// SORTSIGNATURE_S OMIT
-func Sort(data Interface) {
-	// el código que ordena va aqúi
-}
-
-// SORTSIGNATURE_E OMIT
